34dd462768395071b8bb2a56cf16587383113b81
dkamm <davidfkamm@gmail.com>
2022-06-26 17:52:56 -0700

    Add create room modal

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useWSClient } from "contexts/WSContext";
@@ -9,21 +9,28 @@ export const Home = () => {
 
   const nav = useNavigate();
 
-  const createRoom = useCallback(() => {
-    const num = Math.floor(Math.random() * 1000);
-    wsclient.send({
-      type: "createRoom",
-      payload: {
-        userName: "user-" + num,
-        config: {
-          name: "room-" + num,
-          maxSpectators: 10,
-          public: true,
-          gameTime: 4 * 60,
+  const [userName, setUserName] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [maxSpectators, setMaxSpectators] = useState(4);
+  const [privateRoom, setPrivateRoom] = useState(false);
+
+  const createRoom = useCallback(
+    (userName, roomName, maxSpectators, privateRoom) => {
+      wsclient.send({
+        type: "createRoom",
+        payload: {
+          userName: userName,
+          config: {
+            name: roomName,
+            maxSpectators: maxSpectators,
+            private: privateRoom,
+            gameTime: 4 * 60,
+          },
         },
-      },
-    });
-  }, [wsclient]);
+      });
+    },
+    [wsclient]
+  );
 
   const handler = useCallback(
     (message) => {
@@ -56,9 +63,12 @@ export const Home = () => {
       <div className="row-start-2 row-span-2 col-start-4">
         {isConnected && (
           <div className="flex flex-col gap-y-4">
-            <button className="btn btn-primary block" onClick={createRoom}>
+            <label
+              htmlFor="create-room-modal"
+              className="btn btn-primary modal-button text-center"
+            >
               Create New Room
-            </button>
+            </label>
             <form
               className="flex flex-col gap-y-2 mt-16"
               onSubmit={onSubmitJoinRoom}
@@ -82,6 +92,81 @@ export const Home = () => {
             </form>
           </div>
         )}
+      </div>
+      <input type="checkbox" id="create-room-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <label
+            htmlFor="create-room-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <form>
+            <div className="form-control mt-4">
+              <label>
+                <span className="label-text">User Name</span>
+              </label>
+              <input
+                className="input input-bordered flex-grow"
+                type="text"
+                name="userName"
+                autoComplete="off"
+                maxLength={16}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-4">
+              <label>
+                <span className="label-text">Room Name</span>
+              </label>
+              <input
+                className="input input-bordered flex-grow"
+                type="text"
+                name="roomName"
+                autoComplete="off"
+                maxLength={32}
+                onChange={(e) => setRoomName(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-4 w-48">
+              <label className="label">
+                <span className="label-text">Max Spectators</span>
+                <input
+                  type="number"
+                  className="input input-bordered"
+                  min={0}
+                  max={10}
+                  value={maxSpectators}
+                  onChange={(e) => setMaxSpectators(parseInt(e.target.value))}
+                />
+              </label>
+            </div>
+            <div className="form-control mt-4 w-32">
+              <label className="label cursor-pointer">
+                <span className="label-text">Private?</span>
+                <input
+                  type="checkbox"
+                  className="toggle"
+                  checked={privateRoom}
+                  onChange={() => setPrivateRoom(!privateRoom)}
+                />
+              </label>
+            </div>
+            <div className="modal-action">
+              <label
+                htmlFor="create-room-modal"
+                className="btn btn-primary"
+                disabled={!userName.length || !roomName.length}
+                onClick={() => {
+                  createRoom(userName, roomName, maxSpectators, privateRoom);
+                }}
+              >
+                Create
+              </label>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
