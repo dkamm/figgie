df1e8925d7eff838b415668cde6920984deeadf1
dkamm <davidfkamm@gmail.com>
2022-07-20 12:49:52 -0700

    Update changelog

--- frontend/src/pages/Changelog/index.jsx
+++ frontend/src/pages/Changelog/index.jsx
@@ -5,7 +5,8 @@ export const Changelog = () => {
     <div className="w-1/2 mx-auto text-left my-4">
       <h1 className="text-xl font-semibold">Changelog</h1>
       <br />
-      <p>Todo</p>
+      <h2 className="text-base font-semibold">07-20-2022</h2>
+      <p>Initial release!</p>
     </div>
   );
 };
